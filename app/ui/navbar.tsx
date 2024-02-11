c43f6fae21f6eddd4f43d03c34fdda556e59f492
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-11 21:35:45 +0530

    Font Size Fixed

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,7 +2,7 @@ import NavLinks from "@/app/ui/nav-links";
 
 function Navbar() {
   return (
-    <nav className="bg-slate-600 text-white h-20 flex gap-10 justify-center items-center font-sans text-2xl">
+    <nav className="bg-slate-600 text-white h-20 flex gap-10 justify-center items-center font-sans text-sm md:text-2xl">
         <NavLinks />
     </nav>
   )
