1a9a1da4c11b1a5f7ab06dd9e1a5739a557545cd
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-15 21:32:49 +0530

    Added Transports Websocket in socket instance

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -6,7 +6,9 @@ import io from "socket.io-client";
 import TurnIndicator from "../ui/turn-indicator";
 import GameBoard from "../ui/game-board";
 
-const socket = io("https://tic-tac-toe-server-ykw6.onrender.com:5000");
+const socket = io("https://tic-tac-toe-server-ykw6.onrender.com:5000",{
+  transports: ['websocket']
+});
 
 export default function Page() {
   const [gameBoard, setGameBoard] = useState(Array(9).fill(null));
