3251f298a64d940bcb2dc1cc12d0b78f21fc228e
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-16 12:53:49 +0530

    Link Updated

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import TurnIndicator from "../ui/turn-indicator";
 import GameBoard from "../ui/game-board";
 
-const socket = io("wss://tic-tac-toe-server-ykw6.onrender.com:5000");
+const socket = io("wss://tic-tac-toe-server-ykw6.onrender.com");
 
 export default function Page() {
   const [gameBoard, setGameBoard] = useState(Array(9).fill(null));
